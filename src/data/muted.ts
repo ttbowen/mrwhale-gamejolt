e1441243a0579db1539ff4d9bc2501b11ec32b76
bowenwaregames <ttbowen@hotmail.co.uk>
2017-07-23 18:13:51 +0100

    Fix markdown in muted responses

--- src/data/muted.ts
+++ src/data/muted.ts
@@ -1,15 +1,15 @@
 export const fun = [
-    'Whale then <<NAME>>*got muted.',
-    'O__O **<<NAME>>** got muted.',
-    'Oh my **<<NAME>>** got muted.',
-    'The mute hammer has dropped on **<<NAME>>**',
-    'A mighty moderator has muted **<<NAME>>** O__O',
-    'Wait did **<<NAME>>\'s** messages just disappear?',
-    '**<<NAME>>** got muted.... That was unexpected.',
-    'Plot twist. It seems **<<NAME>>** got muted.'
+    'Whale then <<NAME>> got muted.',
+    'O__O <<NAME>> got muted.',
+    'Oh my <<NAME>> got muted.',
+    'The mute hammer has dropped on <<NAME>>',
+    'A mighty moderator has muted <<NAME>> O__O',
+    'Wait did <<NAME>>\'s messages just disappear?',
+    '<<NAME>> got muted.... That was unexpected.',
+    'Plot twist. It seems <<NAME>> got muted.'
 ];
 
 export const serious = [
-    '**<<NAME>>** got muted.',
-    '**<<NAME>>** is now muted.'
+    '<<NAME>> got muted.',
+    '<<NAME>> is now muted.'
 ];

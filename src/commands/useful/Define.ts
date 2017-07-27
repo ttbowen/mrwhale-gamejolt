aed9b68e92e7e6d726e65a4d30c9d810a4d2adca
bowenwaregames <ttbowen@hotmail.co.uk>
2017-07-28 00:06:08 +0100

    Add profanity filtering for definitions.

--- src/commands/useful/Define.ts
+++ src/commands/useful/Define.ts
@@ -2,6 +2,7 @@ import { Message } from 'gamejolt.js';
 import { Command, Permissions, Util } from 'gamejolt-bot';
 
 import * as urban from 'urban';
+import * as profanity from 'profanity-util';
 
 export default class extends Command {
     public constructor() {
@@ -19,11 +20,21 @@ export default class extends Command {
 
         if (!word) return message.reply('You must provide a word to define');
 
+        let replacements: string[] = [
+            'Shrimp',
+            'Whale',
+            'Barnacles',
+            'Barnacle-head',
+            'Fishpaste',
+            '*Dolphin noises*',
+            'Tartar sauce'
+        ]
+
         let search = word.trim().toLowerCase();
 
         if (await this._redis.get(`dictionary::${search}`)) {
             let definition: string = Util.truncate(255, await this._redis.get(`dictionary::${search}`));
-            return message.reply(`${word}: ${definition}`);
+            return message.reply(`${word}: ${profanity.purify(definition, { replace: true, replacementsList: replacements })[0]}`);
         }
 
         const define = urban(word);
@@ -31,7 +42,7 @@ export default class extends Command {
         define.first((json) => {
             if (json) {
                 let definition: string = Util.truncate(255, json.definition);
-                return message.reply(`${word}: ${definition}`);
+                return message.reply(`${word}: ${profanity.purify(definition, { replace: true, replacementsList: replacements })[0]}`);
             } else {
                 return message.reply(`Could not define this.`);
             }
